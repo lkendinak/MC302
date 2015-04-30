20a5fc0ef1b969c1b6e2e29dfa431fe74fcd11b6
Lucas Kendi <lucas.kendi@evolucard.com.br>
2015-04-30 00:50:08 -0300

    Lab07 Done!

--- src/Ativ7/MultithreadSorter.java
+++ src/Ativ7/MultithreadSorter.java
@@ -1,78 +1,116 @@
 package Ativ7;
 
-
 public class MultithreadSorter<T> implements Runnable {
 
+	// Teto para execução do bubbleSort
 	private final int M = 7;
+
+	// Lista a ser ordenada
 	private T[] list;
+
+	// Comparator utilizado
 	private Comparator<T> comp;
+
+	// Posições inicial e final para a ordenação do vetor
 	private int ini, fim;
-	
+
+	// Construtor inicial
 	public MultithreadSorter(T[] lista, Comparator<T> comparator) {
 		this.list = lista;
 		this.comp = comparator;
 	}
-	
-	public MultithreadSorter(T[] lista, int i, int j) {
+
+	// Overload do construtor inicial, modificado apra receber os indices
+	// inicial e final da lista
+	public MultithreadSorter(T[] lista, Comparator<T> comparator, int i, int j) {
 		this.list = lista;
+		this.comp = comparator;
 		this.ini = i;
 		this.fim = j;
 	}
 
+	// Método sort() inicial
 	public void sort() throws InterruptedException {
-		Thread t1 = new Thread(new MultithreadSorter<T>(list, comp));
+		// Os valores ini e fim são inicializados para a primeira chamada que
+		// utiliza o construtor inicial
+		this.ini = 0;
+		this.fim = this.list.length - 1;
+
+		// Inicia a primeira thread
+		Thread t1 = new Thread(new MultithreadSorter<T>(list, comp, this.ini,
+				this.fim));
 		t1.start();
 		t1.join();
 	}
 
+	// Override do método run(), que executa sempre que uma thread é startada,
+	// executa o quicksort concorrente com os parâmetros inicializados no
+	// construtor
 	@Override
 	public void run() {
-		quickSort(this.list, 0, this.list.length - 1);
+		try {
+			quickSort(this.list, this.ini, this.fim);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
-	private void quickSort(T[] array, int start, int end) {
-		T pivot = array[start]; // consider this as hole at inList[start],
-		int leftPointer = start;
-		int rightPointer = end;
-		final int LEFT = 1;
-		final int RIGHT = -1;
-		int pointerSide = RIGHT; // we start with right as pivot is from left
+	private void quickSort(T[] array, int start, int end)
+			throws InterruptedException {
+		int left = start;
+		int right = end;
 
-		while (leftPointer != rightPointer) {
-			if (pointerSide == RIGHT) {
-				if (this.comp.precede(array[rightPointer], pivot)) {
-					array[leftPointer] = array[rightPointer];
-					leftPointer++;
-					pointerSide = LEFT;
-				} else {
-					rightPointer--;
-				}
-			} else if (pointerSide == LEFT) {
-				if (this.comp.precede(pivot, array[leftPointer])) {
-					array[rightPointer] = array[leftPointer];
-					rightPointer--;
-					pointerSide = RIGHT;
-				} else {
-					leftPointer++;
-				}
+		if (array.length < M) {
+			bubbleSort(array, start, end);
+			return;
+		}
+		T pivot = array[(left + right) / 2];
+
+		while (left <= right) {
+
+			// Enquanto o valor apontado por left for menor que o pivot,
+			// atualiza o ponteiro
+			while (this.comp.precede(array[left], pivot)) {
+				left++;
+			}
+
+			// Equanto o valor apontado por right for maior que o pivot,
+			// atualiza o ponteiro
+			while (this.comp.precede(pivot, array[right])) {
+				right--;
+			}
+
+			// Se encontrarmos algum valor na lista esquerda que é maior que o
+			// pivot e se encontrarmos um valor na lista direita que é menor que
+			// o pivot entao nós trocamos os valores, e em seguida atuliza os
+			// ponteiros
+			if (left <= right) {
+				swap(list, left, right);
+				left++;
+				right--;
 			}
 		}
 
-		// put the pivot where leftPointer and rightPointer collide i.e.
-		// leftPointer == rightPointer
-		array[leftPointer] = pivot;
+		// Cria uma thread nova para a lista esquerda
+		if (start < right) {
+			Thread ts1 = new Thread(new MultithreadSorter<T>(list, comp, start,
+					right));
+			ts1.start();
 
-		if ((leftPointer - start) > M) {
-			quickSort(array, start, leftPointer - 1);
-
-		} else {
-			bubbleSort(array, start, leftPointer - 1);
+			// O join é necessário pois como na chamada recursiva, a thread deve
+			// terminar de executar para podermos garantir a corretude dos dados
+			// antes de executarmos para a lista direita
+			ts1.join();
 		}
 
-		if ((end - leftPointer) > M) {
-			quickSort(array, leftPointer + 1, end);
-		} else {
-			bubbleSort(array, leftPointer + 1, end);
+		// Cria uma thread nova para a lista direita
+		if (left < end) {
+			Thread ts2 = new Thread(new MultithreadSorter<T>(list, comp, left,
+					end));
+			ts2.start();
+			// O join é necessário pois como na chamada recursiva, a thread deve
+			// terminar de executar para podermos garantir a corretude dos dados
+			ts2.join();
 		}
 
 	}
